2f51e647424967090895122f20399b1658c9cdce
bramlak <kalmarbalint97@gmail.com>
2026-01-22 13:35:14 +0100

    fix(grafana): move grafana ui config files outside argocd app path

---
+++ grafana-config/create-configmaps.sh
@@ -0,0 +1,6 @@
+if ! command -v kubectl >/dev/null 2>&1; then
+  echo "Kubectl is not installed"
+  exit 1
+fi
+
+kubectl create configmap grafana-dashboard --from-file=node-dashboard.json --namespace monitoring --labels="grafana_dashboard:1"
--- platform/grafana/create-configmaps.sh
+++
@@ -1,6 +0,0 @@
-if ! command -v kubectl >/dev/null 2>&1; then
-  echo "Kubectl is not installed"
-  exit 1
-fi
-
-kubectl create configmap grafana-dashboard --from-file=test-dashboard.json --namespace monitoring
