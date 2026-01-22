265dbc56c886844bc69bb51fe808bba39cd2b4aa
bramlak <kalmarbalint97@gmail.com>
2026-01-22 15:13:36 +0100

    feat(grafana): add dashboard json flow

--- grafana-config/create-configmaps.sh
+++ grafana-config/create-configmaps.sh
@@ -3,4 +3,4 @@ if ! command -v kubectl >/dev/null 2>&1; then
   exit 1
 fi
 
-kubectl create configmap grafana-dashboard --from-file=node-dashboard.json --namespace monitoring --labels="grafana_dashboard:1"
+kubectl create configmap grafana-dashboard --from-file=node-dashboard.json --namespace monitoring --overrides='{"metadata":{"label":"grafana_dashboard": "1"}}'
